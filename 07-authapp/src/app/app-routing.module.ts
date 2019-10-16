31f6c3eee865e6e6d06f1a734c1b2350fb558828
desfaceldi17 desfaceldi17 <desfaceldi17@desfaceldi17.jaimetorres.local>
2019-10-16 11:47:56 -0500

    Finalizacion seccion 07-authapp

--- 07-authapp/src/app/app-routing.module.ts
+++ 07-authapp/src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
 import { PeciosComponent } from './components/pecios/pecios.component';
 import { HomeComponent } from './components/home/home.component';
+import { CallbackComponent } from './components/callback/callback.component';
+import { AuthGuard } from './services/auth.guard';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'precios', component: PeciosComponent },
-  { path: 'protegida', component: ProtegidaComponent },
+  {
+    path: 'protegida',
+    component: ProtegidaComponent,
+    canActivate: [AuthGuard]
+  },
+  { path: 'callback', component: CallbackComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
