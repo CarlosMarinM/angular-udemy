c5d4049338c1175d4bd2564dd4d3e99bb34ddaeb
Carlos <carlos.marinm@hotmail.com>
2019-11-19 21:08:07 -0500

    Seguimos con seccion 12

--- 12-mapas/src/app/components/mapa/mapa.component.ts
+++ 12-mapas/src/app/components/mapa/mapa.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Marcador } from '../../classes/marcador.class';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MapaEditarComponent } from './mapa-editar.component';
+
 
 @Component({
   selector: 'app-mapa',
@@ -13,7 +17,7 @@ export class MapaComponent implements OnInit {
   lat = 51.678418;
   lng = 7.809007;
 
-  constructor() {
+  constructor(public snackBar: MatSnackBar, public dialog: MatDialog) {
     if (localStorage.getItem('marcadores')) {
       this.marcadores = JSON.parse(localStorage.getItem('marcadores'));
     }
@@ -27,6 +31,7 @@ export class MapaComponent implements OnInit {
     const nuevoMarcador = new Marcador(coords.lat, coords.lng);
     this.marcadores.push(nuevoMarcador);
     this.guardarStorage();
+    this.snackBar.open('Marcador agregado', 'Cerrar', { duration: 3000 });
   }
 
   guardarStorage() {
@@ -36,6 +41,27 @@ export class MapaComponent implements OnInit {
   borrarMarcador(index: number) {
     this.marcadores.splice(index, 1);
     this.guardarStorage();
+    this.snackBar.open('Marcador borrado', 'Cerrar', { duration: 3000 });
+  }
+
+  editarMarcador(marcador: Marcador) {
+    const dialogRef = this.dialog.open(MapaEditarComponent, {
+      width: '250px',
+      data: { titulo: marcador.titulo, desc: marcador.desc }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+
+      if (!result) {
+        return;
+      }
+
+      marcador.titulo = result.titulo;
+      marcador.desc = result.desc;
+      this.guardarStorage();
+      this.snackBar.open('Marcador actualizado', 'Cerrar', { duration: 3000 });
+    });
   }
 
 }
